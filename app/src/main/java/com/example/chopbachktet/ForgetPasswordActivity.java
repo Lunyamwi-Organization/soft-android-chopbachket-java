33a41ec716c9c8b0276a8b162e74a16216b45bd2
AsinineFatuity <asininefatuity@gmail.com>
2020-04-22 15:00:06 +0300

    user can now reset their password afterverifying phone number & answering security questions

--- app/src/main/java/com/example/chopbachktet/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/chopbachktet/ForgetPasswordActivity.java
@@ -1,8 +1,10 @@
 package com.example.chopbachktet;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -69,8 +71,18 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         else if (check.equals("login"))
         {
             phoneNumber.setVisibility(View.VISIBLE);
+            verifyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                   verifyUser();
+                }
+            });
         }
     }
+
+
+
     private void setSecurityAnswers()
     {
         String answer1=question1.getText().toString().toLowerCase();
@@ -105,7 +117,7 @@ public class ForgetPasswordActivity extends AppCompatActivity {
             });
         }
     }
-    //retrieeve previous answers
+    //retrieve previous answers
     private void displaySecurityQuestions()
     {
         DatabaseReference securityRef= FirebaseDatabase.getInstance()
@@ -133,5 +145,120 @@ public class ForgetPasswordActivity extends AppCompatActivity {
             }
         });
     }
+    private void verifyUser()
+    {
+      //get the input fields from user first
+        final String answer1=question1.getText().toString().toLowerCase();
+        final String answer2=question2.getText().toString().toLowerCase();
+        final String phoneNum=phoneNumber.getText().toString();
+        //verify if the above parameters are null otherwise you get a crash
+        if(!phoneNum.equals("")&& !answer1.equals("")&& !answer2.equals(""))
+        {
+
+            final DatabaseReference securityRef= FirebaseDatabase.getInstance()
+                    .getReference().child("Users")
+                    .child(phoneNum);//because user is not logged in already so we get it from the edit text
+            //check whether user and security questions exist
+            securityRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                {
+                    if(dataSnapshot.exists())
+                    {
+                        //retrieve phone number from database
+                        String mPhone=dataSnapshot.child("phone").getValue().toString();
+                        //no need to check in case users shipment number is not equal to their unique key phone number since they are not logged in
+                        //check whether the security questions are there
+                        if(dataSnapshot.hasChild("Security Questions"))
+                        {
+                            //retrieve answers for comparison go into the security questions node, add another child
+                            String ans1= dataSnapshot.child("Security Questions").child("Answer1").getValue().toString();
+                            String ans2= dataSnapshot.child("Security Questions").child("Answer2").getValue().toString();
+                            if(!ans1.equals(answer1))
+                            {
+                                Toast.makeText(ForgetPasswordActivity.this,"First answer is incorrect",Toast.LENGTH_SHORT).show();
+                            }
+                            else if(!ans2.equals(answer2))
+                            {
+                                Toast.makeText(ForgetPasswordActivity.this,"Second answer is incorrect",Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                //allow user to change password
+                                AlertDialog.Builder builder=new AlertDialog.Builder(ForgetPasswordActivity.this);
+                                builder.setTitle("New Password");
+                                //new edit text for the new password entry
+                                final EditText newPassword=new EditText(ForgetPasswordActivity.this);
+                                newPassword.setHint("write new password here");
+                                builder.setView(newPassword);
+                                //
+                                builder.setPositiveButton("Reset", new DialogInterface.OnClickListener()
+                                {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        if(!newPassword.getText().toString().equals(""))
+                                        {
+                                            securityRef.child("password")
+                                                    .setValue(newPassword.getText().toString())
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>()
+                                                    {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task)
+                                                        {
+                                                            if(task.isSuccessful())
+                                                            {
+                                                                Toast.makeText(ForgetPasswordActivity.this,"password reset successfully",Toast.LENGTH_SHORT).show();
+                                                                //send user to the login screen
+                                                                Intent intent = new Intent(ForgetPasswordActivity.this, LoginActivity.class);
+                                                                startActivity(intent);
+                                                            }
+                                                        }
+                                                    });
+                                        }
+                                        //naughty users with empty password fields
+                                        else
+                                            {
+                                                Toast.makeText(ForgetPasswordActivity.this,"Password field cannot be empty",Toast.LENGTH_SHORT).show();
+                                            }
+                                    }
+                                });
+                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        dialog.cancel();
+                                    }
+                                });
+                                builder.show();
+                            }
+                        }
+                        else
+                        {
+                            //found no security questions node in their account
+                            Toast.makeText(ForgetPasswordActivity.this,"You did not set security questions, please contact customer care",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else
+                    {
+                        //if user inputs a wrong number
+                        Toast.makeText(ForgetPasswordActivity.this,"The user phone number does not exist",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError)
+                {
+
+                }
+            });
+        }
+        //in case any of the fields are empty
+        else
+            {
+                Toast.makeText(ForgetPasswordActivity.this,"Please fill all the fields",Toast.LENGTH_SHORT).show();
+            }
     }
+
+}
 
